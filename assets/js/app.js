52ec93cff8c091cee937f14c5143b67b84bcbbcf
goswaminitesh002-oss <goswaminitesh002@gmail.com>
2026-07-11 12:10:25 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-y
+/* =====================================
+   SMARTGYM AI - MAIN JAVASCRIPT
+===================================== */
+
+const menuBtn = document.querySelector(".menu-btn");
+const navMenu = document.querySelector(".nav-menu");
+
+menuBtn.addEventListener("click", () => {
+
+    navMenu.classList.toggle("active");
+
+    if(navMenu.classList.contains("active")){
+        menuBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    }else{
+        menuBtn.innerHTML = '<i class="fa-solid fa-bars"></i>';
+    }
+
+});
